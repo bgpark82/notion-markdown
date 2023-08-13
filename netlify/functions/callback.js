c87675ad786909c98f41168edffc949d9d9b7e0f
musinsa-bgpark <peter.park@musinsa.com>
2023-08-13 16:13:38 +0900

    chore: 캐시 httpOnly 제거

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -3,13 +3,12 @@ const url = require('url')
 exports.handler = async (event, context) => {
     const obj = url.parse(event.rawUrl, true)
     const code = obj.query.code
-    console.log(code);
 
     return {
         statusCode: 200,
         body: code,
         headers: {
-            'Set-Cookie': `myCookie=${code}; Max-Age=3600; Path=/; HttpOnly;`,
+            'Set-Cookie': `tmp=${code}; Max-Age=86400;`,
             'Content-Type': 'text/plain'
         },
     }
